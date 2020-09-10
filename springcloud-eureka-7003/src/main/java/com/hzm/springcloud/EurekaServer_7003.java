a547278479934691a99fadbecfb7c35423d5dda5
hzm131 <891453178@qq.com>
2020-09-10 14:10:17 +0800

    集çeureka¾¤配置

---
+++ springcloud-eureka-7003/src/main/java/com/hzm/springcloud/EurekaServer_7003.java
@@ -0,0 +1,13 @@
+package com.hzm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer // 开启Eureka服务  可以接收别人的注册
+public class EurekaServer_7003 {
+    public static void main(String[] arg){
+        SpringApplication.run(EurekaServer_7003.class,arg);
+    }
+}
